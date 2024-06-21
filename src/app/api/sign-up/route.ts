a5ec404ba50fa8164b117c9f1723a8719fd59c3f
MuhammadAsad-cmd <ma6530028@gmail.com>
2024-06-21 10:42:51 +0500

    no

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,2 +1,17 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+  } catch (error) {
+    console.error("Error registering user", error);
+    return Response.json({
+      success: false,
+      message: "Error",
+    });
+  }
+}
